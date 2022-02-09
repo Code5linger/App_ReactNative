f6234130242205a30b228a09c405251be686fe55
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2022-02-09 14:51:15 +0600

    Card Design

--- codeslingerV1/v1/App.js
+++ codeslingerV1/v1/App.js
@@ -2,19 +2,66 @@ import React from "react";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import styled from "styled-components";
+import Card from "./components/Card";
 
 export default function App() {
   return (
     <Container /*style={styles.container}*/>
-      <Text>My First App ‼</Text>
-      <StatusBar style="auto" />
+      <TitleBar>
+        <Avatar source={require("./assets/avatar.jpg")}></Avatar>
+        <Title>Welcome Back,</Title>
+        <Name>Code</Name>
+        <StatusBar style="auto" />
+      </TitleBar>
+      <Subtitle>Continue Learning</Subtitle>
+      <Card />
     </Container>
   );
 }
 
+const Subtitle = styled.Text`
+  color: #b8bece;
+  font-weight: 600;
+  font-size: 15px;
+  margin-left: 20px;
+  margin-top: 50px;
+  text-transform: uppercase;
+`;
+
+const Avatar = styled.Image`
+  width: 44px;
+  height: 44px;
+  background: black;
+  border-radius: 22px;
+  margin-left: 20px;
+  position: absolute;
+  top: 0;
+  left: 0;
+`;
+
 const Container = styled.View`
   flex: 1;
   background-color: #f0f3f5;
+  /*
   justify-content: center;
   align-items: center;
+  */
+`;
+
+const Title = styled.Text`
+  font-size: 16px;
+  color: #b8bece;
+  font-weight: 500;
+`;
+
+const Name = styled.Text`
+  font-size: 20px;
+  color: #3c4560;
+  font-weight: bold;
+`;
+
+const TitleBar = styled.View`
+  width: 100%;
+  margin-top: 58px;
+  padding-left: 80px;
 `;
