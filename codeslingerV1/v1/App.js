2d7ab05503d2662274918667e34b413fb3b63c98
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2022-02-09 13:53:41 +0600

    Setting Up

--- codeslingerV1/v1/App.js
+++ codeslingerV1/v1/App.js
@@ -1,21 +1,20 @@
 import React from "react";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import styled from "styled-components";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>My First App!</Text>
+    <Container /*style={styles.container}*/>
+      <Text>My First App ‼</Text>
       <StatusBar style="auto" />
-    </View>
+    </Container>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+const Container = styled.View`
+  flex: 1;
+  background-color: #f0f3f5;
+  justify-content: center;
+  align-items: center;
+`;
