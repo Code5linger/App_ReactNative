d969367528d912a632ab891f3f50188f50e9219f
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2022-02-11 16:57:03 +0600

    Giving Up on V1😞

--- codeslingerV1/v1/screens/HomeScreen.js
+++ codeslingerV1/v1/screens/HomeScreen.js
@@ -1,88 +1,167 @@
+import { Ionicons } from "@expo/vector-icons";
 import React from "react";
-import { StatusBar } from "expo-status-bar";
-import { ScrollView, SafeAreaView } from "react-native";
+import {
+  ScrollView,
+  SafeAreaView,
+  TouchableOpacity,
+  Animated,
+  Easing,
+  StatusBar,
+} from "react-native";
 import styled from "styled-components";
 import Card from "../components/Card";
-import { Ionicons } from "@expo/vector-icons";
+
+import { NotificationIcon } from "../components/Icons";
 import Logo from "../components/Logo";
 import Course from "../components/Course";
 import Menu from "../components/Menu";
+import { connect } from "react-redux";
 
-export default function HomeScreen() {
-  return (
-    <Container>
-      <Menu />
-      <SafeAreaView>
-        <ScrollView style={{ height: "100%" }}>
-          <TitleBar>
-            <Avatar source={require("../assets/avatar.jpg")}></Avatar>
-            <Title>Welcome Back,</Title>
-            <Name>Code</Name>
-            <Ionicons
-              name="ios-notifications"
-              size={32}
-              color="#4775f2"
-              style={{ position: "absolute", right: 20, top: 5 }}
-            />
-
-            <StatusBar style="auto" />
-          </TitleBar>
-          <ScrollView
-            style={{
-              flexDirection: "row",
-              padding: 20,
-              paddingLeft: 12,
-              paddingTop: 30,
-            }}
-            horizontal={true}
-            showsHorizontalScrollIndicator={false}
-          >
-            {logos.map((logo, index) => (
-              <Logo key={index} image={logo.image} text={logo.text} />
-            ))}
-          </ScrollView>
-          <Subtitle>Continue Learning</Subtitle>
-          <ScrollView
-            horizontal={true}
-            style={{ paddingBottom: 30 }}
-            showsHorizontalScrollIndicator={false}
-          >
-            {cards.map((card, index) => (
-              <Card
-                key={index}
-                title={card.title}
-                image={card.image}
-                caption={card.caption}
-                logo={card.logo}
-                subtitle={card.subtitle}
-              />
-            ))}
-          </ScrollView>
-          <Subtitle>Popular Courses</Subtitle>
-          {courses.map((course, index) => (
-            <Course
-              key={index}
-              image={course.image}
-              title={course.title}
-              subtitle={course.subtitle}
-              logo={course.logo}
-              author={course.author}
-              avatar={course.avatar}
-              caption={course.caption}
-            />
-          ))}
-        </ScrollView>
-      </SafeAreaView>
-    </Container>
-  );
+function mapStateToProps(state) {
+  return { action: state.action };
 }
+
+function mapDispatchToProps(dispatch) {
+  return {
+    openMenu: () =>
+      dispatch({
+        type: "OPEN_MENU",
+      }),
+  };
+}
+
+class HomeScreen extends React.Component {
+  state = {
+    scale: new Animated.Value(1),
+    opacity: new Animated.Value(1),
+  };
+
+  componentDidMount() {
+    StatusBar.setBarStyle("dark-content", true);
+  }
+
+  componentDidUpdate() {
+    this.toggleMenu();
+  }
+
+  toggleMenu = () => {
+    if (this.props.action == "openMenu") {
+      Animated.timing(this.state.scale, {
+        toValue: 0.9,
+        duration: 300,
+        easing: Easing.in(),
+      }).start();
+      Animated.spring(this.state.opacity, {
+        toValue: 0.5,
+      }).start();
+
+      StatusBar.setBarStyle("light-content", true);
+    }
+
+    if (this.props.action == "closeMenu") {
+      Animated.timing(this.state.scale, {
+        toValue: 1,
+        duration: 300,
+        easing: Easing.in(),
+      }).start();
+      Animated.spring(this.state.opacity, {
+        toValue: 1,
+      }).start();
+
+      StatusBar.setBarStyle("dark-content", true);
+    }
+  };
+
+  render() {
+    return (
+      <RootView>
+        <Menu />
+        <AnimatedContainer
+          style={{
+            transform: [{ scale: this.state.scale }],
+            opacity: this.state.opacity,
+          }}
+        >
+          <SafeAreaView>
+            <ScrollView style={{ height: "100%" }}>
+              <TitleBar>
+                <TouchableOpacity
+                  onPress={this.props.openMenu}
+                  style={{ position: "absolute", top: 0, left: 20 }}
+                >
+                  <Avatar source={require("../assets/avatar.jpg")} />
+                </TouchableOpacity>
+                <Title>Welcome back,</Title>
+                <Name>Meng</Name>
+                <NotificationIcon
+                  style={{ position: "absolute", right: 20, top: 5 }}
+                />
+              </TitleBar>
+              <ScrollView
+                style={{
+                  flexDirection: "row",
+                  padding: 20,
+                  paddingLeft: 12,
+                  paddingTop: 30,
+                }}
+                horizontal={true}
+                showsHorizontalScrollIndicator={false}
+              >
+                {logos.map((logo, index) => (
+                  <Logo key={index} image={logo.image} text={logo.text} />
+                ))}
+              </ScrollView>
+              <Subtitle>Continue Learning</Subtitle>
+              <ScrollView
+                horizontal={true}
+                style={{ paddingBottom: 30 }}
+                showsHorizontalScrollIndicator={false}
+              >
+                {cards.map((card, index) => (
+                  <Card
+                    key={index}
+                    title={card.title}
+                    image={card.image}
+                    caption={card.caption}
+                    logo={card.logo}
+                    subtitle={card.subtitle}
+                  />
+                ))}
+              </ScrollView>
+              <Subtitle>Popular Courses</Subtitle>
+              {courses.map((course, index) => (
+                <Course
+                  key={index}
+                  image={course.image}
+                  title={course.title}
+                  subtitle={course.subtitle}
+                  logo={course.logo}
+                  author={course.author}
+                  avatar={course.avatar}
+                  caption={course.caption}
+                />
+              ))}
+            </ScrollView>
+          </SafeAreaView>
+        </AnimatedContainer>
+      </RootView>
+    );
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(HomeScreen);
+
+const RootView = styled.View`
+  background: black;
+  flex: 1;
+`;
 
 const Subtitle = styled.Text`
   color: #b8bece;
   font-weight: 600;
   font-size: 15px;
   margin-left: 20px;
-  margin-top: 20px;
+  margin-top: 10px;
   text-transform: uppercase;
 `;
 
@@ -91,16 +170,15 @@ const Avatar = styled.Image`
   height: 44px;
   background: black;
   border-radius: 22px;
-  margin-left: 20px;
-  position: absolute;
-  top: 0;
-  left: 0;
 `;
 
 const Container = styled.View`
   flex: 1;
   background-color: #f0f3f5;
+  border-radius: 10px;
 `;
+
+const AnimatedContainer = Animated.createAnimatedComponent(Container);
 
 const Title = styled.Text`
   font-size: 16px;
@@ -116,7 +194,7 @@ const Name = styled.Text`
 
 const TitleBar = styled.View`
   width: 100%;
-  margin-top: 58px;
+  margin-top: 50px;
   padding-left: 80px;
 `;
 
@@ -130,20 +208,20 @@ const logos = [
     text: "Figma",
   },
   {
-    image: require("../assets/logo-invision.png"),
-    text: "Invision",
+    image: require("../assets/logo-studio.png"),
+    text: "Studio",
   },
   {
     image: require("../assets/logo-react.png"),
     text: "React",
   },
   {
-    image: require("../assets/logo-sketch.png"),
-    text: "Sketch",
+    image: require("../assets/logo-swift.png"),
+    text: "Swift",
   },
   {
-    image: require("../assets/logo-studio.png"),
-    text: "Studio",
+    image: require("../assets/logo-sketch.png"),
+    text: "Sketch",
   },
 ];
 
@@ -183,7 +261,7 @@ const courses = [
     title: "Prototype in InVision Studio",
     subtitle: "10 sections",
     image: require("../assets/background13.jpg"),
-    logo: require("./assets/logo-studio.png"),
+    logo: require("../assets/logo-studio.png"),
     author: "Meng To",
     avatar: require("../assets/avatar.jpg"),
     caption: "Design and interactive prototype",
