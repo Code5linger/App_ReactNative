18a72059ca0a0d7550131639f871462e174b221c
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2022-02-14 23:04:38 +0600

    Styling Navbar

--- codeslingerV1/v3/App.js
+++ codeslingerV1/v3/App.js
@@ -1,25 +1,61 @@
 import React from "react";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Text } from "react-native";
+import styled from "styled-components";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Testing🧪</Text>
+    <Container>
+      <TitleBar>
+        <Avatar source={require("./assets/avatar.jpg")} />
+        <Title>Welcome back,</Title>
+        <Name>Code</Name>
+      </TitleBar>
+      <Subtitle>Continue Learning</Subtitle>
       <StatusBar style="auto" />
-    </View>
+    </Container>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+const Subtitle = styled.View`
+  color: #b8bece;
+  font-weight: 600;
+  font-size: 15px;
+  margin-left: 20px;
+  margin-top: 50px;
+  text-transform: uppercase;
+`;
 
-/**
- * TODO:
- */
+const Avatar = styled.Image`
+  width: 44px;
+  height: 44px;
+  background: black;
+  border-radius: 22px;
+  margin-left: 20px;
+  position: absolute;
+  top: 0;
+  left: 0;
+`;
+
+const Container = styled.View`
+  flex: 1;
+  background-color: #f0f3f5;
+`;
+
+const Title = styled.Text`
+  font-size: 16px;
+  color: #b8bece;
+  font-weight: 500;
+`;
+
+const Name = styled.Text`
+  font-size: 20px;
+  color: #3c4560;
+  font-weight: bold;
+`;
+
+const TitleBar = styled.View`
+  width: 100%;
+  margin-top: 50px;
+  padding-left: 80px;
+`;
