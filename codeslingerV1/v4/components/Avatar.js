28ede7046567b728670dae8cff027c1403431257
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2022-02-16 15:54:26 +0600

    API

---
+++ codeslingerV1/v4/components/Avatar.js
@@ -0,0 +1,16 @@
+import React from "react";
+import styled from "styled-components";
+
+class Avatar extends React.Component {
+  state = {
+    photo: ""
+  }
+
+  render() {
+    return <Image />;
+  }
+}
+
+export default Avatar;
+
+const Image = styled.Image``;
