9fcc3879f267647a5800892d8d5de348d298d901
Sakib Ahmed <sakib.ahmed.1708@gmail.com>
2022-02-16 19:07:15 +0600

    Fetching API

--- codeslingerV1/v4/components/Avatar.js
+++ codeslingerV1/v4/components/Avatar.js
@@ -1,20 +1,45 @@
 import React from "react";
 import styled from "styled-components";
 
+function mapStateToProps(state) {
+  return {
+    name: state.name,
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    updateName: (name) =>
+      dispatch({
+        type: "UPDATE_NAME",
+        name: name,
+      }),
+  };
+}
+
 class Avatar extends React.Component {
   state = {
-    photo: "https://i.ibb.co/PCc0b7R/avatar-default.jpg",
+    photo: "https://cl.ly/55da82beb939/download/avatar-default.jpg",
   };
 
-  componentDidCatch() {
-    fetch("https://api.github.com/users/defunkt/avatar_url").then((response) =>
-      response.json().then((response) => {
+  componentDidMount() {
+    fetch("https://uifaces.co/api?limit=1&random", {
+      headers: new Headers({
+        "X-API-KEY": "DD072B9B-407E4AD9-9FC647EE-B87B8230",
+      }),
+    })
+      .then((response) => response.json())
+      .then((response) => {
+        console.log(response);
+
         this.setState({
-          photo: response.photo,
+          photo: response[0].photo,
         });
-      })
-    );
+
+        this.props.updateName(response[0].name);
+      });
   }
+
   render() {
     return <Image source={{ uri: this.state.photo }} />;
   }
